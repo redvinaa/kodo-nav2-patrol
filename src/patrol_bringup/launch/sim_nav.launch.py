ad1805fe1c4f5afd1440d2e8886e25ed01c0a89f
Vince Reda <redvinaa@gmail.com>
2026-04-11 23:46:10 +0200

    Cleanup

--- src/patrol_bringup/launch/sim_nav.launch.py
+++ src/patrol_bringup/launch/sim_nav.launch.py
@@ -1,18 +1,4 @@
-# Copyright (c) 2018 Intel Corporation
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""This is all-in-one launch script intended for use by nav2 developers."""
+# Based on nav2_bringup/tb3_simulation_launch.py
 
 import os
 
@@ -154,7 +140,7 @@ def generate_launch_description():
 
     declare_robot_sdf_cmd = DeclareLaunchArgument(
         'robot_sdf',
-        default_value=os.path.join(simulation_dir, 'config', 'waffle.model'),
+        default_value=os.path.join(bringup_dir, 'worlds', 'waffle.model'),
         description='Full path to robot sdf file to spawn the robot in gazebo')
 
     # Specify the actions
